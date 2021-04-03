5fde1da4c17282a49f4c33686c383f8a38dadd56
fernandoboza <hi@fernandoboza.com@users.noreply.github.com>
2021-04-03 15:26:37 -0400

    working on linked list

---
+++ Data-Structures/LinkedList.ts
@@ -0,0 +1,45 @@
+class Node<T> {
+    constructor(public data: T = null, public next: Node<T> = null){}
+}
+
+export default class SinglyLinkedList <T>{
+    constructor(init?: T){
+        this._root = new Node(init);
+    }
+    private _root: Node<T>;
+
+    public size = 0;
+
+    get root(): Node<T> {
+        return this._root;
+    }
+    private traverse<T>(){}
+
+    public insertInBegin(data: T) {
+        this.size++;
+    }
+    public insertInEnd(data: T) {
+        const newNode = new Node<T>(data);
+        if(!!this._root) {
+            this._root = newNode;
+        }
+
+        // if there are already nodes present
+        let current =  this._root;
+        while(current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+        this.size++;
+        return this._root
+    }
+    public deleteNode(node: T) {
+        this.size--;
+    }
+    public search(data: T) {}
+}
+
+const ll = new SinglyLinkedList();
+ll.insertInEnd(10)
+// ll.insertInEnd(20)
+ll
