ded93bba3e78e09071fdb37d5c6e0816a51d662a
fernandoboza <17830527+FernandoBoza@users.noreply.github.com>
2022-03-16 14:46:04 -0400

    added sample.ts

---
+++ sample.ts
@@ -0,0 +1,5 @@
+import BinarySearch from './Algorithms/Search/BinarySearch'
+
+BinarySearch([ 3, 9, 10, 27, 38, 43, 82 ], 3)
+BinarySearch([ 'a', 'b', 'c', 'e','z' ], "e")
+BinarySearch([ 'a', 'b', 'c', 'e','z' ], "f")
