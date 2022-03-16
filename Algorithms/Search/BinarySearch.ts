f27d5ecb000aacab4b09c8cb793b8e627e310c5b
fernandoboza <17830527+FernandoBoza@users.noreply.github.com>
2022-03-16 14:42:15 -0400

    long overdue update

--- Algorithms/Search/BinarySearch.ts
+++ Algorithms/Search/BinarySearch.ts
@@ -21,6 +21,3 @@ export default function BinarySearch<T>(list: T[], key: T): T | number {
     return -1;
 }
 
-BinarySearch([ 3, 9, 10, 27, 38, 43, 82 ], 3)
-BinarySearch([ 'a', 'b', 'c', 'e','z' ], "e")
-BinarySearch([ 'a', 'b', 'c', 'e','z' ], "f")
